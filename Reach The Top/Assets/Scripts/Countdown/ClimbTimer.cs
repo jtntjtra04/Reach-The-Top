0cf6b50b19b43feb5c1069f715464abb9ffe3710
KuroPika04 <justintjitra@gmail.com>
2025-06-10 04:49:11 +0800

    bug fixing and final result for demo (still shitty)

--- Reach The Top/Assets/Scripts/Countdown/ClimbTimer.cs
+++ Reach The Top/Assets/Scripts/Countdown/ClimbTimer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class ClimbTimer : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class ClimbTimer : MonoBehaviour
 
     // References
     public FinishUI ui_manager;
+    public DynamicMoveProvider move_provider;
+    private void Start()
+    {
+        countdown_text.gameObject.SetActive(false);
+    }
 
     private void Update()
     {
@@ -32,11 +38,21 @@ public class ClimbTimer : MonoBehaviour
     }
     public void StopClimbing()
     {
+        StartCoroutine(ShowResultTimer(3f));
+    }
+    private IEnumerator ShowResultTimer(float time)
+    {
         if (is_climbing)
         {
             is_climbing = false;
             countdown_text.gameObject.SetActive(false);
-            ui_manager.ShowFinishUI(climb_time);
+            // Lock Movement
+            if (move_provider != null)
+            {
+                move_provider.enabled = false;
+            }
         }
+        yield return new WaitForSeconds(time);
+        ui_manager.ShowFinishUI(climb_time);
     }
 }
